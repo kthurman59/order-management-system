e3b91de43e03746a2832d9b5cab018217753c62f
kthurman59 <kthurman59@gmail.com>
2025-10-02 05:13:51 -0500

    chore: cleanup project structure, ensure standard Spring Boot layout

---
+++ oms-service/src/test/java/com/kevdev/oms/oms_service/OrderManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kevdev.oms.oms_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
